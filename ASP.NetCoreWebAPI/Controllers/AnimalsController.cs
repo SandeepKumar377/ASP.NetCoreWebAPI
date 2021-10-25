770d0790c9c9aeefe6336637e3409005aa2d9f48
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-25 21:16:14 +0530

    Retrun_200_status_code_ok

---
+++ ASP.NetCoreWebAPI/Controllers/AnimalsController.cs
@@ -0,0 +1,25 @@
+﻿using ASP.NetCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        public IActionResult GetAnimals()
+        {
+            var animals = new List<AnimalModel>()
+            {
+                new AnimalModel() {Id=1, Name="Dog"},
+                new AnimalModel() {Id=2, Name="Tigar"}
+            };
+            return Ok(animals);
+        }
+    }
+}
