d11afe439eae0e4670eadb9fa61404f466dd8493
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-25 14:25:20 +0530

    Specific Type

---
+++ ASP.NetCoreWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using ASP.NetCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //return specific data
+
+        //public EmployeeModel GetEmployees()
+        //{
+        //    return new EmployeeModel() { Id=1, Name="Empoluee1"};
+        //}
+
+
+        //return list 
+
+        //public List<EmployeeModel> GetEmployees()
+
+        //use List<EmployeeModel> or IEnumerable<EmployeeModel> and for async data IAsyncEnumerable<EmployeeModel>
+        public IEnumerable<EmployeeModel> GetEmployees()
+
+        {
+            return new List<EmployeeModel>() {
+                new EmployeeModel(){
+                    Id = 1,
+                    Name = "Empoluee1"
+                },
+                new EmployeeModel(){
+                    Id = 2,
+                    Name = "Empoluee2"
+                }
+            };
+        }
+    }
+}
