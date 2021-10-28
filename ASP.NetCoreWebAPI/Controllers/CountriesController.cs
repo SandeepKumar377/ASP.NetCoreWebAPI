5e47ac361e9ae683f9f5c9d77163fdfaefe7fcbd
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-28 16:56:30 +0530

    [BindProperty] attribute_ Bind Incoming form-data to public properties

---
+++ ASP.NetCoreWebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,26 @@
+﻿using ASP.NetCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+       //[BindProperty]           //For httpPost Method
+       [BindProperty(SupportsGet =true)]           //For httpGet Method
+       public CountryModel Country { get; set; }
+
+        //[HttpPost("")]
+        [HttpGet("")]
+        public IActionResult AddCountry()
+        {
+            return Ok($"Name = {this.Country.Name}, Population = {this.Country.Population}, Area = {this.Country.Area}");
+        }
+    }
+}
---
+++ ASP.NetCoreWebAPI/Models/CountryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreWebAPI.Models
+{
+    public class CountryModel
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public int Area { get; set; }
+    }
+}
