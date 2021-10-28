33076c55b38b03fe8bef8e32245f53f246712d15
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-29 00:43:49 +0530

    Custom Model Binder

--- ASP.NetCoreWebAPI/Controllers/CountriesController.cs
+++ ASP.NetCoreWebAPI/Controllers/CountriesController.cs
@@ -21,5 +21,13 @@ namespace ASP.NetCoreWebAPI.Controllers
         {
             return Ok($"Id = {id}, Name = {name}, Age = {age}");
         }
+
+        //Custom Model Binder url ex-/api/countries/search?countries=india|china|US
+
+        [HttpGet("search")]
+        public IActionResult SearchCountries([ModelBinder(typeof(CustomBinder))]string[] countries)
+        {
+            return Ok(countries);
+        }
     }
 }
