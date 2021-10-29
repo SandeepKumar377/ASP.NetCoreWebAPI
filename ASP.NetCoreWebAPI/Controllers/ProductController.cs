888cb544db71375eb50a4d959705d065378fe2b7
Sandeep Kumar <sandeepkumar.sk5.ss55@gmail.com>
2021-10-30 01:34:11 +0530

    Working with Services without using Dependency Injection

---
+++ ASP.NetCoreWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using ASP.NetCoreWebAPI.Models;
+using ASP.NetCoreWebAPI.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository _productRepository;
+        public ProductController()
+        {
+            _productRepository = new ProductRepository();
+        }
+
+        [HttpPost("")]
+        public IActionResult AddProduct([FromBody] ProductModel product)
+        {
+            _productRepository.AddProduct(product);
+            var products = _productRepository.GetAllProducts();
+            return Ok(products);
+        }
+    }
+}
---
+++ ASP.NetCoreWebAPI/Repository/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using ASP.NetCoreWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetCoreWebAPI.Repository
+{
+    public class ProductRepository
+    {
+        private List<ProductModel> products = new List<ProductModel>();
+        public int AddProduct(ProductModel product)
+        {
+            product.Id=products.Count+1;
+            products.Add(product);
+            return product.Id;
+        }
+
+        public List<ProductModel> GetAllProducts()
+        {
+            return products;
+        }
+    }
+}
